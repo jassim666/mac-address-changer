4eb386fae43c58635460b1ef8ea730c80ebbb5b3
jassim666 <100210005+jassim666@users.noreply.github.com>
2022-02-22 10:29:49 -0500

    Update and rename README.md to changer.py

---
+++ changer.py
@@ -0,0 +1,5 @@
+# mac-address-changer
+import subprocess
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
